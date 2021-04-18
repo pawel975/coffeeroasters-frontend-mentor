5a9e6884653b45b7360a6b891e248a557d8e18ad
Pawel <pawel.kurek975@gmail.com>
2021-04-18 22:45:13 +0200

    project final

--- src/Components/HamburgerMenu.js
+++ src/Components/HamburgerMenu.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styled from 'styled-components';
 import logo from '../assets/shared/desktop/logo.svg';
 import {Link} from 'react-router-dom'
-import close from '../assets/shared/mobile/icon-close.svg'
+import close from '../assets/shared/mobile/icon-close.svg';
+import {HamburgerContext} from '../Components/CoffeeRoasterContext';
 
 const HamburgerMenuWrapper = styled.div`
     width:100vw;
@@ -31,7 +32,9 @@ const HamburgerMenuWrapper = styled.div`
         background-color:#FEFCF7;
 
         .modal-nav {
-            outline:2px solid green;
+            padding:1em 0.2em;
+            padding-bottom:3em;
+            /* outline:2px solid green; */
             display:flex;
             align-items:center;
             justify-content:space-between;
@@ -43,32 +46,67 @@ const HamburgerMenuWrapper = styled.div`
             img.logo {
                 height:18px;
             }
+             
+        }
+
+        .links-wrapper {
+        display:flex;
+        flex-direction:column;
+        justify-content:space-between;
+        list-style:none;
+        padding:0;
+        margin:0;
+        height:150px;
+        padding-bottom:3em;
+        box-shadow: 0px 50px 40px rgb(254, 252, 247);
+        
+        .link {
+            text-decoration:none;
+        }
+
+        
+        li {
+            text-align:center;
+            font-family:"Fraunces", serif;
+            /* outline: 2px solid green; */
+            color:#333D4B;
+            font-weight:bold;
+            font-size:24px;
+            line-height:32px;
+
+            &:hover {
+                color:silver;
+            }
         }
     }
+}
 
-
-
-
-    
 `
 
 const HamburgerMenu = () => {
+
+    const handleModalClose = () => {
+        setHamburgerModalActive(false)
+    }
+
+    const [hamburgerModalActive, setHamburgerModalActive] = useContext(HamburgerContext)
+
     return(
         <HamburgerMenuWrapper>
-            <div className="modal-background"></div>
+            <div onClick={handleModalClose}className="modal-background"></div>
             <div className='modal-content'>
                 <div className="modal-nav">
                     <img className="logo" src={logo} alt="" />
-                    <img className="close" src={close} alt="" /> 
+                    <img onClick={handleModalClose} className="close" src={close} alt="" /> 
                 </div>  
                 <ul className="links-wrapper">
-                    <Link className="link" to="/">
+                    <Link onClick={handleModalClose} className="link" to="/">
                         <li>HOME</li>
                     </Link>
-                    <Link className="link" to="/about">
+                    <Link onClick={handleModalClose} className="link" to="/about">
                         <li>ABOUT US</li>
                     </Link>
-                    <Link className="link" to="/plan">
+                    <Link onClick={handleModalClose} className="link" to="/plan">
                         <li>CREATE YOUR PLAN</li>
                     </Link>
                 </ul>
